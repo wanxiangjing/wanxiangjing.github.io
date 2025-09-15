8b2da7c8c17c4cc803b4afb503c01956fd2bcca9
DongJianHao123 <2995251733@qq.com>
2025-09-15 15:30:32 +0800

    Updates

--- assets/rtc-DHiMLrT-.js
+++
@@ -1,1 +0,0 @@
-import{c}from"./index-DsxKIGXk.js";const e={getGuideScene:async()=>c.post("/rtc/guide/getScenes"),guidStartVoiceChat:async t=>c.post("/rtc/guide/proxy?Action=StartVoiceChat",t),guidStopVoiceChat:async t=>c.post("/rtc/guide/proxy?Action=StopVoiceChat",t)};export{e as r};
